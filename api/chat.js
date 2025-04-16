bd1777a27f294ed8fa0ab113b78003dead6ac730
Vít Pergler <vit.pergler@tirecheck.com>
2025-04-16 21:46:50 +0200

    add log

--- api/chat.js
+++ api/chat.js
@@ -39,7 +39,7 @@ export const config = {
           temperature: 0.7
         })
       });
-  
+      console.info("openaiRes", openaiRes);
       const data = await openaiRes.json();
   
       if (!data.choices || !data.choices[0]) {
